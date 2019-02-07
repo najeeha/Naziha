03f5769add4a8c7fff770a2b6c674ee1ef4c064f
najeeha <42862372+najeeha@users.noreply.github.com>
2019-02-07 14:26:54 +0530

    Update nazivowel.py

--- nazivowel.py
+++ nazivowel.py
@@ -1,6 +1,8 @@
 ch = input(" ")
 if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
  or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+    print("vowels")
+  elif(ch!>'a' or ch!<'z' or ch!>'A' or ch!<'z'):
     print("invalid")
-else:
+  else:
     print("Consonant")
