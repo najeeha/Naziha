87b04c196302fb5641d890802677f50ff783ada4
najeeha <42862372+najeeha@users.noreply.github.com>
2019-02-19 14:24:23 +0530

    Update factorial2.py

--- factorial2.py
+++ factorial2.py
@@ -1,10 +1,10 @@
-num = int(input())
+r = int(input())
 factorial = 1
-if num < 0:
+if r < 0:
    print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
+elif r== 0:
    print("The factorial of 0 is 1")
 else:
-   for i in range(1,num + 1):
+   for i in range(1,r+ 1):
        factorial = factorial*i
    print(factorial)
